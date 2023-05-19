000bd9f535f010e7a42c7126573f89a18548ebc7
Nilesh Srivastava <80592367+blackeye-prog@users.noreply.github.com>
2023-05-20 01:46:25 +0530

    upload all files

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full flex-col'>
+      <p>Home</p>
+      Made by Nilesh Srivastava
+    </div>
+  )
+}
+
+export default Home
